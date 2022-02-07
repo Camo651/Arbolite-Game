865ef9247d24b6d22b6e08393840c28d83eb2468
Camo <47070087+camocreepercast@users.noreply.github.com>
2022-02-06 20:29:29 -0500

    q456yyq46hgwesv23
    hgq3weefqq4

--- Assets/Scripts/InterfaceManager.cs
+++ Assets/Scripts/InterfaceManager.cs
@@ -9,4 +9,32 @@ public class InterfaceManager : MonoBehaviour
 	[HideInInspector] public GlobalRefManager globalRefManager;
 	public Image backgroundBlur;
 	public UserInterface activeUserInterface;
+	public Queue<UserInterface> activeNotificationQueue, pastNotificationsQueue;
+
+
+	private void Start()
+	{
+		InitializeUserInterface();
+	}
+
+	// sets all the default or player pref values for what all the interfaces should look like on startup of the main game
+	private void InitializeUserInterface()
+	{
+		SetBackgroundBlur(false);
+	}
+
+
+	//adds a notification to the end of the stream of notifications to be seen
+	public void EnqueueNotification(string title, string descript, int upTime, bool toBeSaved)
+	{
+
+	}
+	public void DequeueNotification()
+	{
+
+	}
+	public void SetBackgroundBlur(bool state)
+	{
+		backgroundBlur.enabled = state;
+	}
 }
--- Assets/Scripts/UserInterface.cs
+++ Assets/Scripts/UserInterface.cs
@@ -6,13 +6,27 @@ using UnityEngine.UI;
 public class UserInterface : MonoBehaviour
 {
 	// any UI panel in game
+	public InterfaceManager interfaceManager;
 	public Image interfaceBackground;
 	public InterfaceType interfaceType;
+	[HideInInspector] public bool interfaceLocked; //to be set on init
+	public string interfaceName;
+
+
+
+
+
+
+
+
+
+
 	public enum InterfaceType
 	{
 		FullScreen, //blur bkg, takes up whole screen
 		HUD, // dont blur, persitent overlay when in game view
 		Modal, //blur bkg, smaller interface
-		WorldSpace // dont blur bkg, not persistent, floats around in world space
+		WorldSpace, // dont blur bkg, not persistent, floats around in world space
+		Notification // dont blur, not persistent, queses in the notification bar
 	}
 }
